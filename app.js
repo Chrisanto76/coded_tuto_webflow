dd69619a75c54c40ad86cf3d8b6dadadbc0ca4b6
Crhisanto76 <antonypoure2@gmail.com>
2023-05-31 16:21:04 +0200

    add responsve design and add form block

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ const majorButton = document.querySelector(".major-button");
 majorButton.onclick = function() {
     alert('Welcome on board!!');
 }
+
+function toggleMenu() {
+    var navbarCollapse = document.getElementById("navbarCollapse");
+    navbarCollapse.classList.toggle("show");
+  }
+  
